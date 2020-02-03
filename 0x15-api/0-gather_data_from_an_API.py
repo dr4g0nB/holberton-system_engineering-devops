c331b9b0e4b94184c9f296640dd054cd64c00ae3
bxrbxrx <barbaracalle10i3@gmail.com>
2020-02-03 16:11:56 -0500

    Pep8 happ

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,8 +6,10 @@ import sys
 if __name__ == "__main__":
     employee_id = sys.argv[1]
     """ Information from json placeholder converted in json format """
-    ph_users = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)).json()
-    ph_todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)).json()
+    ph_users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(employee_id)).json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(employee_id)).json()
     """ Empty list to fill in with the todos list """
     all_tasks = []
     """  Traversing the todos list """
@@ -17,7 +19,8 @@ if __name__ == "__main__":
             """ Appending the content of the dict to the empty list """
             all_tasks.append(looking_tasks.get("title"))
     """ Printing the dictionary """
-    print("Employee {} is done with tasks({}/{}):".format(ph_users.get("name"), len(all_tasks), len(ph_todos)))
-    # Filtering down to only show the tasks 
+    print("Employee {} is done with tasks({}/{}):"
+          .format(ph_users.get("name"), len(all_tasks), len(todos)))
+    """ Filtering down to only show the tasks """
     for looking_tasks in all_tasks:
         print("\t {}".format(looking_tasks))
