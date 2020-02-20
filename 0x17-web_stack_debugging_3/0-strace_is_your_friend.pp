f38f01a6d71ece46a108a4fa7b32d214d6fa59d4
dabrabgellak <barbaracalle10i3@gmail.com>
2020-02-20 03:17:18 +0000

    dabuging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Finds out why Apache is returning a 500 error.
+file { '/var/www/html/wp-includes/class-wp-locale.phpp':
+  ensure => file,
+   source => '/var/www/html/wp-includes/class-wp-locale.php',
+}
