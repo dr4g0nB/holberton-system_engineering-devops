212b943b9b8102a68fb5a566a0080265602956c9
dabrabgellak <barbaracalle10i3@gmail.com>
2020-03-02 17:05:59 +0000

    Removed one word from the comment

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 # Fixing request
-exec { 'Fixes request':
+exec { 'Fixes':
   command => '/bin/echo ULIMIT="-n 32768" | sudo tee /etc/default/nginx',
 }
 
-exec { 'Restart nginx':
+exec { 'Restart' :
   command => '/usr/bin/service nginx restart'
 }
